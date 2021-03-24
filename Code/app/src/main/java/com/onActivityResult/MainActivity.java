528efd7ea51d797b67fbbdfe7320f28816360c66
VinayTS <vinay@succorfish.com>
2021-03-24 17:07:41 +0530

    Basic implementation onActivity Result is completed.

--- Code/app/src/main/java/com/onActivityResult/MainActivity.java
+++ Code/app/src/main/java/com/onActivityResult/MainActivity.java
@@ -10,8 +10,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-  private TextView nameTexView, ageTextView, phoneNumberTextView, sexxTextView;
-  private   Button navigateButton;
+    private TextView nameTexView, ageTextView, phoneNumberTextView, sexxTextView;
+    private Button navigateButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +21,7 @@ public class MainActivity extends AppCompatActivity {
         onClickButton();
         makeTextInvisible();
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-    }
+
     private void intializeViews() {
         /**
          * TextViews
@@ -38,20 +36,40 @@ public class MainActivity extends AppCompatActivity {
         navigateButton = (Button) findViewById(R.id.activity_button_navigate);
 
     }
-    private void makeTextInvisible(){
+
+    private void makeTextInvisible() {
         nameTexView.setVisibility(View.INVISIBLE);
         ageTextView.setVisibility(View.INVISIBLE);
         phoneNumberTextView.setVisibility(View.INVISIBLE);
         sexxTextView.setVisibility(View.INVISIBLE);
     }
-    private void onClickButton(){
+
+    private void onClickButton() {
         navigateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
-                startActivity(intent);
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivityForResult(intent,10);
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 10) {
+            if (resultCode == RESULT_OK) {
+                String result = data.getStringExtra("NAME");
+                System.out.println(result);
+            }
+            if (resultCode == RESULT_CANCELED) {
+
+            }
+        }
+    }
+
+    /**
+     * Link:-
+     * https://stackoverflow.com/questions/10407159/how-to-manage-startactivityforresult-on-android
+     */
 }
--- Code/app/src/main/java/com/onActivityResult/SecondActivity.java
+++ Code/app/src/main/java/com/onActivityResult/SecondActivity.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-  private   TextView nameTexView, ageTextView, phoneNumberTextView, sexxTextView;
+  private EditText nameTexView, ageTextView, phoneNumberTextView, sexxTextView;
   private   Button navigateButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +23,32 @@ public class SecondActivity extends AppCompatActivity {
         /**
          * TextViews
          */
-        nameTexView = (TextView) findViewById(R.id.name_id_textView);
-        ageTextView = (TextView) findViewById(R.id.age_id_textView);
-        phoneNumberTextView = (TextView) findViewById(R.id.phoneNUmber_id_textView);
-        sexxTextView = (TextView) findViewById(R.id.sex_id_textView);
+        nameTexView = (EditText) findViewById(R.id.name_id_editText);
+        ageTextView = (EditText) findViewById(R.id.age_id_editText);
+        phoneNumberTextView = (EditText) findViewById(R.id.phoneNUmber_id_editText);
+        sexxTextView = (EditText) findViewById(R.id.sex_id_editText);
         /**
          * Buttons
          */
         navigateButton = (Button) findViewById(R.id.activity_button_navigate);
-
     }
     private void onClickButton(){
         navigateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(SecondActivity.this,MainActivity.class);
-                startActivity(intent);
+                String name=nameTexView.getText().toString();
+                String age=ageTextView.getText().toString();
+                String phoneNumber=phoneNumberTextView.getText().toString();
+                String sex=sexxTextView.getText().toString();
+                Intent intent=new Intent();
+                intent.putExtra("NAME",name);
+                intent.putExtra("AGE",age);
+                intent.putExtra("PH",phoneNumber);
+                intent.putExtra("SEX",sex);
+                setResult(RESULT_OK,intent);
+                finish();
+              //  Intent intent=new Intent(SecondActivity.this,MainActivity.class);
+                //startActivity(intent);
             }
         });
     }
