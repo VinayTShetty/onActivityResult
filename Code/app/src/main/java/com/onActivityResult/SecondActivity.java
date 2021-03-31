efb9e0b464c33cdcada0250642429e0b3ce31001
VinayTS <vinay@succorfish.com>
2021-04-01 05:22:54 +0530

    Proper UI Validation is Completed

--- Code/app/src/main/java/com/onActivityResult/SecondActivity.java
+++ Code/app/src/main/java/com/onActivityResult/SecondActivity.java
@@ -7,17 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class SecondActivity extends AppCompatActivity {
-  private EditText nameTexView, ageTextView, phoneNumberTextView, sexxTextView;
+public class SecondActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+  private EditText nameTexView, ageTextView, phoneNumberTextView;
   private   Button navigateButton;
+  RadioButton male_RadioButton,female_RadioButton;
+  RadioGroup radioGroup_mf;
+  String male_female="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         intializeViews();
         onClickButton();
+        registerOnClicListner();
     }
     private void intializeViews() {
         /**
@@ -26,11 +34,16 @@ public class SecondActivity extends AppCompatActivity {
         nameTexView = (EditText) findViewById(R.id.name_id_editText);
         ageTextView = (EditText) findViewById(R.id.age_id_editText);
         phoneNumberTextView = (EditText) findViewById(R.id.phoneNUmber_id_editText);
-        sexxTextView = (EditText) findViewById(R.id.sex_id_editText);
         /**
          * Buttons
          */
         navigateButton = (Button) findViewById(R.id.activity_button_navigate);
+        male_RadioButton=(RadioButton)findViewById(R.id.radio_button_male_id);
+        female_RadioButton=(RadioButton)findViewById(R.id.radio_button_female_id);
+        radioGroup_mf=(RadioGroup)findViewById(R.id.radio_group_mf_id);
+    }
+    private void registerOnClicListner(){
+        radioGroup_mf.setOnCheckedChangeListener(this);
     }
     private void onClickButton(){
         navigateButton.setOnClickListener(new View.OnClickListener() {
@@ -39,15 +52,26 @@ public class SecondActivity extends AppCompatActivity {
                 String name=nameTexView.getText().toString();
                 String age=ageTextView.getText().toString();
                 String phoneNumber=phoneNumberTextView.getText().toString();
-                String sex=sexxTextView.getText().toString();
                 Intent intent=new Intent();
+                if(male_female.equalsIgnoreCase("")){
+                    Toast.makeText(getApplicationContext(),"Please Select Gender",Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 intent.putExtra(getResources().getString(R.string.NAME),name);
                 intent.putExtra(getResources().getString(R.string.AGE),age);
                 intent.putExtra(getResources().getString(R.string.PHONE_NO),phoneNumber);
-                intent.putExtra(getResources().getString(R.string.SEX),sex);
+                intent.putExtra(getResources().getString(R.string.SEX),male_female);
                 setResult(RESULT_OK,intent);
                 finish();
             }
         });
+    }
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(male_RadioButton.getId()==checkedId){
+                    male_female="M";
+                }else if(female_RadioButton.getId()==checkedId){
+                    male_female="F";
+                }
     }
 }
